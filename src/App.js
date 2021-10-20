7100e81d6e5a6b596326ca48ae9584503ebe828e
RavinduSathsara <rsathsara330@gmail.com>
2021-10-20 17:15:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  //format result
+  const formatCount = () => {
+    return count === 0 ? "Zero" : count;
+  };
+  //Color format
+  const colorFormat = () => {
+    return count === 0
+      ? "badge m-2 badge-warning"
+      : count < 0
+      ? "badge m-2 badge-danger"
+      : "badge m-2 badge-primary";
+  };
+  const removeCount = () => {
+    setCount(count - 1);
+  };
+  const resetCount = () => {
+    setCount(0);
+  };
+  const addCount = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count app</h1>
+      <span className={colorFormat()} style={{ fontSize: 30 }}>
+        {formatCount()}
+      </span>
+      <hr />
+      <button className="btn btn-danger m-3 " onClick={removeCount}>
+        Dicreesment
+      </button>
+      <button className="btn btn-warning m-3" onClick={resetCount}>
+        Reset
+      </button>
+      <button className="btn btn-primary m-3" onClick={addCount}>
+        Increesment
+      </button>
     </div>
   );
 }
